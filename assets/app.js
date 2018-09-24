c3695efdc5d6acb7ac9eadfe969476478321d591
Tom Liang <tomliangg@gmail.com>
2018-09-23 20:54:29 -0700

    fixed the edit modal

--- assets/app.js
+++ assets/app.js
@@ -2,12 +2,11 @@ var app2 = new Vue({
     el: "#vue-prepare",
     delimiters: ['${', '}'],
     data: {
-        msg: 'test',
         orders: [],
         loading: false,
         currentOrder: {},
         message: null,
-        newOrder: { 'article_heading': null, 'article_body': null },
+        newOrder: { 'name': null, 'coffee_order': null },
       },
     mounted: function() {
         this.getOrders();
@@ -32,9 +31,10 @@ var app2 = new Vue({
           },
           getOrder: function(id) {
               this.loading = true;
-              this.$http.get(`/food/orderdetail/${id}`)
+              this.$http.get(`/food/orderdetail/${id}/`)
                 .then((response) => {
                     this.currentOrder = response.data;
+                    $("#editOrderModal").modal('show');
                     this.loading = false;
                 })
                 .catch((err) => {
@@ -47,6 +47,19 @@ var app2 = new Vue({
             this.$http.delete(`/food/orderdetail/${id}/`, {headers: {'X-CSRFToken': this.getCookie('csrftoken')}} )
                 .then((response) => {
                     this.loading = false;
+                    this.getOrders();
+                })
+                .catch((err) => {
+                    this.loading = false;
+                    console.log(err);
+                })
+          },
+          updateOrder: function() {
+              this.loading = true;
+              this.$http.put(`food/orderdetail/${this.currentOrder.id}/`, this.currentOrder)
+                .then((response) => {
+                    this.loading = false;
+                    this.currentOrder = response.data;
                     this.getOrders();
                 })
                 .catch((err) => {
